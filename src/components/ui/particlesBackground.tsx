aceeb5e047b8c7df6702684f6976531c28926cdd
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2025-04-12 00:18:36 -0300

    refactor: remove FilterBar component

--- src/components/ui/particlesBackground.tsx
+++ src/components/ui/particlesBackground.tsx
@@ -267,6 +267,7 @@ export function ParticlesBackground({
       }
       cancelAnimationFrame(animationFrameRef.current);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     particleCount,
     particleColor,
